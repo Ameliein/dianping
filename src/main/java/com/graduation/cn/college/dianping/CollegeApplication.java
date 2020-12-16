c237bb36f1e31ab6c2626e1e9993046bc4d477e6
Amelie <ameliein@163.com>
2020-12-16 21:27:32 +0800

    ElasticSearch & mysql Evaluation: Alibaba Canal set up

--- src/main/java/com/graduation/cn/college/dianping/CollegeApplication.java
+++ src/main/java/com/graduation/cn/college/dianping/CollegeApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.graduation.cn.college.dianping"})
 @MapperScan("com.graduation.cn.college.dianping.dal")
 @EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableScheduling
 public class CollegeApplication {
 
     public static void main(String[] args) {
